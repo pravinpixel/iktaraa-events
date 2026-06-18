45598aae5b8ccef1c3398058fee806f4619f4138
yenok-edison <yenok@iktaraa.com>
2026-06-18 15:24:09 +0530

    Nagma sarai 2 event update

--- events.js
+++ events.js
@@ -1,19 +1,18 @@
 const EVENTS = {
-  kaapi: {
-    id: "kaapi",
-    name: "Kaapi Ragam",
-    price: "₹300 per ticket",
-    poster: "images/kaapi_ragam_7.png",
-    // paymentBtnId: "pl_SBxkmmq9QMFkIQ"
-    paymentBtnId: "pl_SR3vtHqKwxgswA"
-  },
-  // nagma: {
-  //   id: "nagma",
-  //   name: "Nagma Saraai",
-  //   price: "₹400 per ticket",
-  //   poster: "images/Nagma.png",
-  //   paymentBtnId: "pl_SBxmzJ6iWpLYkd"
+  // kaapi: {
+  //   id: "kaapi",
+  //   name: "Kaapi Ragam",
+  //   price: "₹300 per ticket",
+  //   poster: "images/kaapi_ragam_7.png",
+  //   paymentBtnId: "pl_SR3vtHqKwxgswA"
   // },
+  nagma: {
+    id: "nagma",
+    name: "Nagma Saraai",
+    price: "₹600 per ticket",
+    poster: "images/nagma_2.png",
+    paymentBtnId: "pl_SBxmzJ6iWpLYkd"
+  },
   // sax: {
     // id: "sax",
     // name: "A Saxophone Ensemble",
